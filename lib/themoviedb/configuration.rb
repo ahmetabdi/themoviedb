c0f24a5ef75694cf18c3f3531cb2c00a76bda781
Ross Noble <rosshnoble@gmail.com>
2014-03-24 20:52:41 -0400

    Create private method for images response

--- lib/themoviedb/configuration.rb
+++ lib/themoviedb/configuration.rb
@@ -11,34 +11,40 @@ module Tmdb
     # Simply combine them all and you will have a fully qualified URL. Here’s an example URL:
     # http://cf2.imgobject.com/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
     def base_url
-      fetch_response['images']['base_url']
+      images_config['base_url']
     end
 
     # HTTPS
     def secure_base_url
-      fetch_response['images']['secure_base_url']
+      images_config['secure_base_url']
     end
 
     def poster_sizes
-      fetch_response['images']['poster_sizes']
+      images_config['poster_sizes']
     end
 
     def backdrop_sizes
-      fetch_response['images']['backdrop_sizes']
+      images_config['backdrop_sizes']
     end
 
     def profile_sizes
-      fetch_response['images']['profile_sizes']
+      images_config['profile_sizes']
     end
 
     def logo_sizes
-      fetch_response['images']['logo_sizes']
+      images_config['logo_sizes']
     end
 
     def fetch_response
       options = @params.merge(Api.config)
       response = Api.get(@resource, :query => options)
       response.to_hash
+    end
+
+    private
+
+    def images_config
+      fetch_response['images'] || {}
     end
   end
 end
