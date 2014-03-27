e3584984cac8ded3a074ed79717d99d1dfa92be9
Pavel Krasnovskij <snusmu@gmail.com>
2014-03-27 16:26:44 +0000

    add access to /discover/ api

--- lib/themoviedb.rb
+++ lib/themoviedb.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'httparty'
 
-["api", "search", "resource", "configuration"].each do |inc|
+["api", "search", "resource", "configuration", "discover"].each do |inc|
   require File.join(File.dirname(__FILE__), "themoviedb", inc)
 end
 
