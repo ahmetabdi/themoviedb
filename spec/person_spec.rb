21f905f4d7b494f57cdbfc10a92de4a82bca7239
Dan K <itsdanya@gmail.com>
2016-01-30 12:32:34 +0300

    Update specs to new syntax

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -20,25 +20,25 @@ describe Tmdb::Person do
 
     it "should return the popular people" do
       VCR.use_cassette 'person/popular' do
-        @person.popular.should be_true
+        expect(@person.popular).to be_truthy
       end
     end
 
     it "should return the latest person" do
       VCR.use_cassette 'person/latest' do
-        @person.latest.should be_true
+        expect(@person.latest).to be_truthy
       end
     end
 
     it "should return credits for a person ID" do
       VCR.use_cassette 'person/credits' do
-        @person.credits(5292).should be_true
+        expect(@person.credits(5292)).to be_truthy
       end
     end
 
     it "should return changes for a person ID" do
       VCR.use_cassette 'person/changes' do
-        @person.changes(5292).should be_true
+        expect(@person.changes(5292)).to be_truthy
       end
     end
   end
@@ -51,43 +51,43 @@ describe Tmdb::Person do
     end
 
     it "should return a id" do
-      @person['id'].should == 5292
+      expect(@person['id']).to eq 5292
     end
 
     it "should return a name" do
-      @person['name'].should == "Denzel Washington"
+      expect(@person['name']).to eq "Denzel Washington"
     end
 
     it "should return a adult" do
-      @person['adult'].should == false
+      expect(@person['adult']).to eq false
     end
 
     it "should return a biography" do
-      @person['biography'].should == "From Wikipedia, the free encyclopedia.\n\nDenzel Hayes Washington, Jr. (born December 28, 1954) is an American actor, screenwriter, director and film producer. He first rose to prominence when he joined the cast of the medical drama St. Elsewhere, playing Dr. Philip Chandler for six years. He has received much critical acclaim for his work in film since the 1990s, including for his portrayals of real-life figures, such as Steve Biko, Malcolm X, Rubin \"Hurricane\" Carter, Melvin B. Tolson, Frank Lucas, and Herman Boone.\n\nWashington has received two Academy Awards, two Golden Globe awards, a Tony Award. He is notable for winning the Best Supporting Actor for Glory in 1989; and the Academy Award for Best Actor in 2001 for his role in the film Training Day\n\nDescription above from the Wikipedia article Denzel Washington, licensed under CC-BY-SA, full list of contributors on Wikipedia."
+      expect(@person['biography']).to eq "From Wikipedia, the free encyclopedia.\n\nDenzel Hayes Washington, Jr. (born December 28, 1954) is an American actor, screenwriter, director and film producer. He first rose to prominence when he joined the cast of the medical drama St. Elsewhere, playing Dr. Philip Chandler for six years. He has received much critical acclaim for his work in film since the 1990s, including for his portrayals of real-life figures, such as Steve Biko, Malcolm X, Rubin \"Hurricane\" Carter, Melvin B. Tolson, Frank Lucas, and Herman Boone.\n\nWashington has received two Academy Awards, two Golden Globe awards, a Tony Award. He is notable for winning the Best Supporting Actor for Glory in 1989; and the Academy Award for Best Actor in 2001 for his role in the film Training Day\n\nDescription above from the Wikipedia article Denzel Washington, licensed under CC-BY-SA, full list of contributors on Wikipedia."
     end
 
     it "should return a birthday" do
-      @person['birthday'].should == "1954-12-28"
+      expect(@person['birthday']).to eq "1954-12-28"
     end
 
     it "should return a deathday" do
-      @person['deathday'].should == ""
+      expect(@person['deathday']).to eq ""
     end
 
     it "should return a homepage" do
-      @person['homepage'].should == ""
+      expect(@person['homepage']).to eq ""
     end
 
     it "should return a place of birth" do
-      @person['place_of_birth'].should == "Mount Vernon, New York, USA"
+      expect(@person['place_of_birth']).to eq "Mount Vernon, New York, USA"
     end
 
     it "should return a profile path" do
-      @person['profile_path'].should == "/khMf8LLTtppUwuZqqnigD2nAy26.jpg"
+      expect(@person['profile_path']).to eq "/khMf8LLTtppUwuZqqnigD2nAy26.jpg"
     end
 
     it "should return an array with also known as names" do
-      @person['also_known_as'].should == []
+      expect(@person['also_known_as']).to eq []
     end
   end
 
@@ -101,33 +101,33 @@ describe Tmdb::Person do
     end
 
     it 'should return movie credits' do
-      @person['movie_credits']['cast'].size.should == 47
-      @person['movie_credits']['cast'].first['id'].should == 388
-      @person['movie_credits']['crew'].size.should == 4
-      @person['movie_credits']['crew'].first['id'].should == 13435
+      expect(@person['movie_credits']['cast'].size).to eq 47
+      expect(@person['movie_credits']['cast'].first['id']).to eq 388
+      expect(@person['movie_credits']['crew'].size).to eq 4
+      expect(@person['movie_credits']['crew'].first['id']).to eq 13435
     end
 
     it 'should return tv credits' do
-      @person['tv_credits']['cast'].size.should == 17
-      @person['tv_credits']['cast'].first['id'].should == 1709
-      @person['tv_credits']['crew'].size.should == 3
-      @person['tv_credits']['crew'].first['id'].should == 18881
+      expect(@person['tv_credits']['cast'].size).to eq 17
+      expect(@person['tv_credits']['cast'].first['id']).to eq 1709
+      expect(@person['tv_credits']['crew'].size).to eq 3
+      expect(@person['tv_credits']['crew'].first['id']).to eq 18881
     end
 
     it 'should return combined credits' do
-      @person['combined_credits']['cast'].size.should == 64
-      @person['combined_credits']['cast'].first['id'].should == 388
-      @person['combined_credits']['crew'].size.should == 7
-      @person['combined_credits']['crew'].first['id'].should == 13435
+      expect(@person['combined_credits']['cast'].size).to eq 64
+      expect(@person['combined_credits']['cast'].first['id']).to eq 388
+      expect(@person['combined_credits']['crew'].size).to eq 7
+      expect(@person['combined_credits']['crew'].first['id']).to eq 13435
     end
 
     it 'should return images' do
-      @person['images']['profiles'].size.should == 6
-      @person['images']['profiles'].first['file_path'].should == '/khMf8LLTtppUwuZqqnigD2nAy26.jpg'
+      expect(@person['images']['profiles'].size).to eq 6
+      expect(@person['images']['profiles'].first['file_path']).to eq '/khMf8LLTtppUwuZqqnigD2nAy26.jpg'
     end
 
     it 'should return changes' do
-      @person['changes']['changes'].should == []
+      expect(@person['changes']['changes']).to eq []
     end
   end
 
